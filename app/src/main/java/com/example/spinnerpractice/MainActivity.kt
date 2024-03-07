32119757f94f0fdab4f64baa0da197c4eff018b1
José Miguel Mejía Crespo <122744449+josem021@users.noreply.github.com>
2024-03-07 17:25:18 -0500

    arreglo de todo

--- app/src/main/java/com/example/spinnerpractice/MainActivity.kt
+++ app/src/main/java/com/example/spinnerpractice/MainActivity.kt
@@ -13,57 +13,48 @@ import android.widget.Spinner
 import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
-    var ButtonPressed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sendSpinner()
-
+        var arrayLanguajes = mutableListOf("Seleccion")
         var textTypingMain = findViewById<EditText>(R.id.editText)
-        var spinnerLanguajes: Spinner = findViewById<Spinner>(R.id.languajesSpinner)
+        val buttonSend = findViewById<Button>(R.id.btnSend)
+        var spinnerLanguajes = findViewById<Spinner>(R.id.languajesSpinner)
+        val intentSpinner = Intent(this, messageLanguaje::class.java)
 
-        val intentSpinner = Intent(this@MainActivity, messageLanguaje::class.java)
 
+        val arrayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayLanguajes)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerLanguajes.adapter = arrayAdapter
+
+        buttonSend.setOnClickListener {
+            var contentTextTypingMain = textTypingMain.text.toString()
+            if (contentTextTypingMain.isNotEmpty()) {
+                if(contentTextTypingMain != " ") {
+                    arrayLanguajes.add(contentTextTypingMain)
+                    arrayAdapter.notifyDataSetChanged()
+                    textTypingMain.text.clear()
+                }
+            }
+        }
 
         spinnerLanguajes.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view:View?, position: Int, id: Long) {
-                if (!ButtonPressed) {
-                    val selectedItem = spinnerLanguajes.selectedItem.toString()
+
+                val selectedItem = spinnerLanguajes.selectedItem.toString()
+                if (selectedItem != "Seleccion") {
                     intentSpinner.putExtra("Seleccion", selectedItem)
                     startActivity(intentSpinner)
                 }
-                ButtonPressed = false
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
 
-        }
-    }
-
-    fun sendSpinner() {
-        var textTypingMain = findViewById<EditText>(R.id.editText)
-        val buttonSend = findViewById<Button>(R.id.btnSend)
-        var spinnerLanguajes: Spinner = findViewById<Spinner>(R.id.languajesSpinner)
-        var arrayLanguajes = mutableListOf<String>()
-        buttonSend.setOnClickListener {
-            var contentTextTypingMain = textTypingMain.text.toString().trim()
-            if (!contentTextTypingMain.isNullOrEmpty()) {
-                if (contentTextTypingMain != " ") {
-                    arrayLanguajes.add(textTypingMain.text.toString())
-                    var arrayAdapter =
-                        ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayLanguajes)
-                    arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                    spinnerLanguajes.adapter = arrayAdapter
-                    spinnerLanguajes.setSelection(arrayLanguajes.size - 1)
-                    ButtonPressed = true
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        ButtonPressed = false
-                    }, 200)
-                }
-            }
         }
     }
 }
