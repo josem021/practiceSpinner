77bd45c204ad4a7ecfb40b8491f3f4d40b170ee4
JoseM021 <122744449+JoseM021@users.noreply.github.com>
2024-03-06 23:08:57 -0500

    Parcialmente terminado

--- app/src/main/java/com/example/spinnerpractice/MainActivity.kt
+++ app/src/main/java/com/example/spinnerpractice/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.spinnerpractice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
@@ -11,6 +13,7 @@ import android.widget.Spinner
 import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
+    var ButtonPressed = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,21 +22,18 @@ class MainActivity : AppCompatActivity() {
 
         var textTypingMain = findViewById<EditText>(R.id.editText)
         var spinnerLanguajes: Spinner = findViewById<Spinner>(R.id.languajesSpinner)
-        var SpinnerTake = false
+
         val intentSpinner = Intent(this@MainActivity, messageLanguaje::class.java)
 
-        spinnerLanguajes.setOnTouchListener{ _, _ ->
-            SpinnerTake = true
-            false
-        }
+
         spinnerLanguajes.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view:View?, position: Int, id: Long) {
-                if (SpinnerTake) {
+                if (!ButtonPressed) {
                     val selectedItem = spinnerLanguajes.selectedItem.toString()
                     intentSpinner.putExtra("Seleccion", selectedItem)
                     startActivity(intentSpinner)
                 }
-
+                ButtonPressed = false
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -57,9 +57,13 @@ class MainActivity : AppCompatActivity() {
                         ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayLanguajes)
                     arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     spinnerLanguajes.adapter = arrayAdapter
+                    spinnerLanguajes.setSelection(arrayLanguajes.size - 1)
+                    ButtonPressed = true
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        ButtonPressed = false
+                    }, 200)
                 }
             }
         }
     }
-
 }
